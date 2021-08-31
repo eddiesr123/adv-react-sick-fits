c9e1b0c35f5f3c34561ec413aacac85ad4a2bd3b
Edward Robinson <eddiesr1234@gmail.com>
2021-08-30 23:11:55 -0400

    added nprogress and server style sheets

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,21 @@
-import Document, { Html, Main, NextScript } from 'next/document';
+/* eslint-disable react/jsx-props-no-spreading */
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html>
-        {/* {Head} */}
+        <Head />
         <body>
           <Main />
           <NextScript />
